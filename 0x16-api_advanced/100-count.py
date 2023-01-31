20df965fa985054164278eae9964af41e2961aae
addisu30 <dabaleaddisu21@outlook.com>
2023-01-31 17:53:22 +0300

    updating

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -70,46 +70,46 @@ def print_words(w, i, len_wn, dict_words_repeated):
 
     print_words(w, i + 1, len_wn, dict_words_repeated)
 
-                                                                                                                                                                                                                                                            return None
+    return None
 
 
-                                                                                                                                                                                                                                                        def get_all_hot_articles(subreddit, word_list, dict_words_unique, after=""):
-                                                                                                                                                                                                                                                                """Get the all articles"""
-                                                                                                                                                                                                                                                                    if after is None:
-                                                                                                                                                                                                                                                                                return dict_words_unique
-                                                                                                                                                                                                                                                                                url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-                                                                                                                                                                                                                                                                                    url += "?limit=100&after={}".format(after)
-                                                                                                                                                                                                                                                                                        headers_m = {'User-agent': 'botsito'}
-                                                                                                                                                                                                                                                                                            redirect = False
+def get_all_hot_articles(subreddit, word_list, dict_words_unique, after=""):
+    """Get the all articles"""
+    if after is None:
+        return dict_words_unique
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url += "?limit=100&after={}".format(after)
+    headers_m = {'User-agent': 'botsito'}
+    redirect = False
 
-                                                                                                                                                                                                                                                                                                r = requests.get(url, allow_redirects=redirect, headers=headers_m)
-                                                                                                                                                                                                                                                                                                    if r.status_code != 200:
-                                                                                                                                                                                                                                                                                                                return None
+    r = requests.get(url, allow_redirects=redirect, headers=headers_m)
+    if r.status_code != 200:
+        return None
 
-                                                                                                                                                                                                                                                                                                                articles = r.json().get("data").get("children")
+    articles = r.json().get("data").get("children")
 
-                                                                                                                                                                                                                                                                                                                    store_title_word_hash(articles, 0, len(articles), dict_words_unique)
+    store_title_word_hash(articles, 0, len(articles), dict_words_unique)
 
-                                                                                                                                                                                                                                                                                                                        after = r.json().get("data").get("after")
-                                                                                                                                                                                                                                                                                                                            return get_all_hot_articles(subreddit, word_list, dict_words_unique, after)
+    after = r.json().get("data").get("after")
+    return get_all_hot_articles(subreddit, word_list, dict_words_unique, after)
 
 
-                                                                                                                                                                                                                                                                                                                        def count_words(subreddit, word_list):
-                                                                                                                                                                                                                                                                                                                                """Count the number of time a word in word_list is name
-                                                                                                                                                                                                                                                                                                                                    in the hot articles subrredt"""
-                                                                                                                                                                                                                                                                                                                                        convert_lower(word_list, 0, len(word_list))
+def count_words(subreddit, word_list):
+    """Count the number of time a word in word_list is name
+    in the hot articles subrredt"""
+    convert_lower(word_list, 0, len(word_list))
 
-                                                                                                                                                                                                                                                                                                                                            words_repeated = {}
-                                                                                                                                                                                                                                                                                                                                                store_given_word_hash(word_list, 0, len(word_list), words_repeated, "r")
+    words_repeated = {}
+    store_given_word_hash(word_list, 0, len(word_list), words_repeated, "r")
 
-                                                                                                                                                                                                                                                                                                                                                    words_unique = {}
-                                                                                                                                                                                                                                                                                                                                                        store_given_word_hash(word_list, 0, len(word_list), words_unique, "u")
+    words_unique = {}
+    store_given_word_hash(word_list, 0, len(word_list), words_unique, "u")
 
-                                                                                                                                                                                                                                                                                                                                                            words_filled = get_all_hot_articles(subreddit, word_list, words_unique)
-                                                                                                                                                                                                                                                                                                                                                                if not words_filled:
-                                                                                                                                                                                                                                                                                                                                                                            return
+    words_filled = get_all_hot_articles(subreddit, word_list, words_unique)
+    if not words_filled:
+        return
 
-                                                                                                                                                                                                                                                                                                                                                                            sort_words = sorted(words_filled.items(), key=lambda t: t[::-1])
-                                                                                                                                                                                                                                                                                                                                                                                sort_des_words = sorted(sort_words, key=lambda tup: tup[1], reverse=True)
+    sort_words = sorted(words_filled.items(), key=lambda t: t[::-1])
+    sort_des_words = sorted(sort_words, key=lambda tup: tup[1], reverse=True)
 
-                                                                                                                                                                                                                                                                                                                                                                                    print_words(sort_des_words, 0, len(sort_des_words), words_repeated)
+    print_words(sort_des_words, 0, len(sort_des_words), words_repeated)
